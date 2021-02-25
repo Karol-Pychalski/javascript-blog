5df957af541f9e54d84fae768cd77e9c8c7decaa
karol Pychalski <k.p201x@gmail.com>
2021-02-25 23:23:04 +0100

    File update

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ function titleClickHandler(event){
   }
 
   /* (działa) add class 'active' to the clicked link */
-
+  
   this.classList.add('active');
   console.log('clickedElement:', clickedElement);
 
@@ -29,21 +29,23 @@ function titleClickHandler(event){
     activeArticle.classList.remove('active');
   }
 
-  /* (poprawiony - czy działa?) get 'href' attribute from the clicked link */
+  console.log('remove class active from all articles is working!', activeArticles);
+  
+  /* (poprawiony - działa) get 'href' attribute from the clicked link */
 
-  const articleSelector = element.getAttribute('href');
+  const articleSelector = clickedElement.getAttribute('href');
 
   console.log('get href is working!', articleSelector);
 
-  /* (czy działa?) find the correct article using the selector (value of 'href' attribute) */
+  /* (działa) find the correct article using the selector (value of 'href' attribute) */
 
-  const targetArticle = element.querySelector('href');
+  const targetArticle = document.querySelector(articleSelector);
 
   console.log('find the correct article is working!', targetArticle);
   
    /* (czy działa?) add class 'active' to the correct article */
 
-  this.targetArticle.add('active');
+  targetArticle.classList.add('active');
 
   console.log('add class active to correct article is working!', targetArticle);
  }
@@ -53,39 +55,3 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
-
-
-/* --- 5.4 --- */
-
-const optArticleSelector = '.post',
-  optTitleSelector = '.post-title',
-  optTitleListSelector = '.titles';
-
-function generateTitleLinks(){
-
-  /* (działa) remove contents of titleList */
-
-  const titleList = document.querySelector(optTitleListSelector).innerHTML = '';
-
-  /* for each article */
-
-  const articles = document.querySelectorAll('.post');
-
-  /* get the article id */
-  for(let article of articles){
-    article.element.getElementById('id');
-  }
-
-    
-
-    /* find the title element */
-
-    /* get the title from the title element */
-
-    /* create HTML of the link */
-
-    /* insert link into titleList */
-
-}
-
-generateTitleLinks();
